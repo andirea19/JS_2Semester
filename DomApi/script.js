a7876185abc9cf9137c4d80bcf5c22c94bfb7e1a
andirea19 <ungerer.andrea@gmail.com>
2024-04-22 21:13:05 +0200

    update

---
+++ DomApi/script.js
@@ -0,0 +1,9 @@
+// JavaScript-Code, um das Modal zu ändern
+document.addEventListener("DOMContentLoaded", function() {
+    
+    // Finde das Element mit der ID "danger-pop-up" und ändere seinen Inhalt
+    var popUp = document.getElementById("danger-pop-up");
+    if (popUp) {
+        popUp.innerHTML = "HAHA, <em>NEUER</em> INHALT!";
+    }
+});
